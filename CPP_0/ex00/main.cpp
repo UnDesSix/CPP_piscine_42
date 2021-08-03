96a2adbd60272896232fd722b6bc6e7b8707fbe9
mlarboul <mlarboul@student.42.fr>
2021-08-03 22:30:42 +0200

    object files

---
+++ CPP_0/ex00/main.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+#include <cstring>
+
+
+std::string uppercase(char *argv)
+{
+    int i(0);
+    std::string tmpString;
+
+    while (argv[i] != '\0')
+    {
+        tmpString += std::toupper(argv[i++]);
+    }
+    return tmpString;
+}
+
+int main(int argc, char **argv)
+{
+    int i(1);
+    std::string finalString;
+
+    if (argc < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    while (i < argc)
+        finalString += uppercase(argv[i++]);
+    std::cout << finalString << std::endl;
+    return (0);
+}
