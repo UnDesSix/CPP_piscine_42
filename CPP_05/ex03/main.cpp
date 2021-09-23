0741776ad561bb460de57bc19656fcd4dde72d3b
UnDesSix <mlarboul@student.42.fr>
2021-09-23 11:25:24 +0200

    adding some new tests

--- CPP_05/ex03/main.cpp
+++ CPP_05/ex03/main.cpp
@@ -10,10 +10,18 @@ int main()
     Intern      you;
     Bureaucrat  president("Emmanuel Macron", 1);
     Bureaucrat  firstMinister("Edouard Philippe", 5);
-    AForm*      unidentified = you.makeForm("Presidential Pardon", "JoeyStarr");
+    AForm*      unidentified1 = you.makeForm("Shrubbery Creation", "JoeyStarr");
+    AForm*      unidentified2 = you.makeForm("Robotomy Request", "JoeyStarr");
+    AForm*      unidentified3 = you.makeForm("Presidential Pardon", "JoeyStarr");
 
-    president.signForm(*unidentified);
-    firstMinister.executeForm(*unidentified);
-    delete unidentified;
+    president.signForm(*unidentified1);
+    firstMinister.executeForm(*unidentified1);
+    delete unidentified1;
+    president.signForm(*unidentified2);
+    firstMinister.executeForm(*unidentified2);
+    delete unidentified2;
+    president.signForm(*unidentified3);
+    firstMinister.executeForm(*unidentified3);
+    delete unidentified3;
     return 0;
 }
